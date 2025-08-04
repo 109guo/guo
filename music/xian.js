c98330ca8d824d5687a3b6b4c41da32209630ed9
琴 <212545318@qq.com>
2025-08-04 16:37:33 +0800

    Add files via upload

---
+++ music/xian.js
@@ -0,0 +1,351 @@
+var userId = "25390779";
+var userServer = "netease";
+var userType = "playlist";
+ var localMusic = [{
+    name: '最浪漫的事',
+      artist: '琴',
+       url: './music/最浪漫的事.mp3',
+      cover: './music/i.png',
+       lrc: './music/最浪漫的事.lrc'
+  },
+   {
+    name: '天使的膀',
+      artist: '馨怡是个好学宝',
+       url: './music/天使的.mp3',
+      cover: './music/i.png',
+       lrc: './music/天使的.lrc'
+ },
+   {
+    name: '爱情讯息',
+      artist: 'HE',
+       url: './music/爱情讯息.flac',
+      cover: './music/i.png',
+       lrc: './music/爱情讯息.lrc'
+ },
+   {
+    name: '后来',
+      artist: 'HE',
+       url: './music/后来.mp3',
+       cover: './music/Yi.png',
+       lrc: './music/后来.lrc'
+ },
+   {
+    name: '孤勇者',
+      artist: '动画剧集中文主题曲',
+       url: './music/孤勇者.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/孤勇者.lrc'
+ },
+   {
+      name: '下辈子还要找你们当爸妈(儿童版）',
+      artist: '分享者最美',
+       url: './music/下辈子还要找你们当爸妈.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/下辈子还要找你们当爸妈.lrc'
+ },
+   {
+    name: '千纸鹤',
+      artist: 'HE',
+       url: './music/千纸鹤.mp3',
+      cover: './music/i.png',
+       lrc: './music/千纸鹤.lrc'
+  },
+   {
+    name: '一生所爱',
+      artist: '琴',
+       url: './music/一生所爱.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/一生所爱.lrc'
+},
+   {
+    name: '春泥',
+      artist: 'HE',
+       url: './music/春泥.mp3',
+      cover: './music/i.png',
+       lrc: './music/春泥.lrc'
+  },
+   {
+    name: '过火',
+      artist: 'HE',
+       url: './music/过火.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/过火.lrc'
+  },
+   {
+    name: '十年',
+      artist: 'HE',
+       url: './music/十年.mp3',
+      cover: './music/i.png',
+       lrc: './music/十年.lrc'
+  },
+   {
+    name: '千年之恋',
+      artist: 'HE',
+       url: './music/千年之恋.mp3',
+      cover: './music/Yi.png',
+       lrc: './music/千年之恋.lrc'
+  },
+   {
+    name: '爱情转移',
+      artist: '馨怡学宝',
+       url: './music/爱情转移.mp3',
+      cover: './music/i.png',
+       lrc: './music/爱情转移.lrc'
+  },
+   {
+    name: '下沙',
+      artist: 'HE',
+       url: './music/下沙.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/下沙.lrc'
+  },
+   {
+    name: '再见深海主题曲',
+      artist: 'YI',
+       url: './music/shai.mp3',
+      cover: './music/i.png',
+       lrc: './music/shai.lrc'
+  },
+   {
+    name: '海浪',
+      artist: 'HE',
+       url: './music/海浪.mp3',
+      cover: './music/Yi.png',
+       lrc: './music/海浪.lrc'
+
+  },
+   {
+    name: '野人男版',
+      artist: 'HE',
+       url: './music/野人男版.mp3',
+      cover: './music/i.png',
+       lrc: './music/野人男版.lrc'
+  },
+   {
+    name: '忘情水',
+      artist: 'HE',
+       url: './music/忘情水.mp3',
+      cover: './music/Yi.png',
+       lrc: './music/忘情水.lrc'
+  },
+   {
+    name: '你的答案',
+      artist: 'YI',
+       url: './music/你的答案.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/你的答案.lrc'
+  },
+   {
+    name: '边界',
+      artist: 'HE',
+       url: './music/边界.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/边界.lrc'
+  },
+   {
+    name: '沧海一声笑',
+      artist: 'HE',
+       url: './music/沧海一声笑.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/沧海一声笑.lrc'
+  },
+   {
+    name: '爱如潮水',
+      artist: 'HE',
+       url: './music/爱如潮水.mp3',
+      cover: './music/Yi.png',
+       lrc: './music/爱如潮水.lrc'
+  },
+   {
+    name: '知否知否',
+      artist: 'QIN',
+       url: './music/知否知否.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/知否知否.lrc'
+  },
+   {
+    name: '离别开出花',
+      artist: 'YI',
+       url: './music/离别开出花.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/离别开出花.lrc'
+  },
+   {
+    name: '为幸福而飞翔',
+      artist: 'HE',
+       url: './music/he.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/he.lrc'
+  },
+   {
+    name: '黄昏',
+      artist: 'HE',
+       url: './music/黄昏.mp3',
+      cover: './music/Yi.png',
+       lrc: './music/黄昏.lrc'
+  },
+   {
+    name: '信仰',
+      artist: 'HE',
+       url: './music/信仰.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/信仰.lrc'
+  },
+   {
+    name: '星辰大海',
+      artist: 'YI',
+       url: './music/星辰大海.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/星辰大海.lrc'
+  },
+   {
+    name: '阳光总在风雨后',
+      artist: '琴',
+       url: './music/阳光总在风雨后.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/阳光总在风雨后.lrc'
+  },
+   {
+    name: '小雪',
+      artist: 'HE',
+       url: './music/小雪.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/小雪.lrc'
+  },
+   {
+    name: '一千零一夜',
+      artist: 'HE',
+       url: './music/一千零一夜.mp3',
+      cover: './music/Yi.png',
+       lrc: './music/一千零一夜.lrc'
+  },
+   {
+    name: '笨小孩',
+      artist: 'HE',
+       url: './music/笨小孩.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/笨小孩.lrc'
+  },
+   {
+    name: '记事本',
+      artist: 'HE',
+       url: './music/记事本.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/记事本.lrc'
+  },
+   {
+    name: '盛夏的果实',
+      artist: 'HE',
+       url: './music/盛夏的果实.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/盛夏的果实.lrc'
+  },
+   {
+    name: '孟婆的碗',
+      artist: 'YI',
+       url: './music/孟婆的碗.mp3',
+      cover: './music/i.png',
+       lrc: './music/孟婆的碗.lrc'
+  },
+   {
+    name: '爱',
+      artist: '琴',
+       url: './music/爱.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/爱.lrc'
+  },
+   {
+    name: '大鱼',
+      artist: 'YI',
+       url: './music/大鱼.mp3',
+      cover: './music/',
+       lrc: './music/大鱼.lrc'
+  },
+   {
+    name: '听我说谢谢你',
+      artist: 'YI',
+       url: './music/听我说谢谢你.mp3',
+      cover: './music/',
+       lrc: './music/听我说谢谢你.lrc'
+  },
+   {
+    name: '少年',
+      artist: 'YI',
+       url: './music/少年.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/少年.lrc'
+  },
+   {
+    name: '跳楼机',
+      artist: 'YI',
+       url: './music/跳楼机.mp3',
+      cover: './music/i.png',
+       lrc: './music/跳楼机.lrc'
+  },
+   {
+    name: '你笑起来真好看',
+      artist: '馨怡是个好学宝',
+       url: './music/你笑起来真好看.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/你笑起来真好看.lrc'
+  },
+   {
+    name: '冰雪奇缘',
+      artist: 'YI',
+       url: './music/冰雪奇缘.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/冰雪奇缘.lrc'
+  },
+   {
+    name: '时光的摇晃',
+      artist: 'YI',
+       url: './music/时光的摇晃.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/时光的摇晃.lrc'
+  },
+   {
+    name: '不知晓',
+      artist: 'YI',
+       url: './music/不知晓.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/不知晓.lrc'
+  },
+   {
+    name: '起风了',
+      artist: 'YI',
+       url: './music/起风了.mp3',
+      cover: './music/H.jpg',
+       lrc: './music/起风了.lrc'
+  },
+   {
+    name: '太多',
+      artist: 'HE',
+       url: './music/太多.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/太多.lrc'
+  },
+   {
+    name: '此去半生',
+      artist: 'HE',
+       url: './music/此去半生.mp3',
+      cover: './music/i.png',
+       lrc: './music/此去半生.lrc'
+  },
+   {
+    name: '桃花诺',
+      artist: 'HE',
+       url: './music/桃花诺.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/桃花诺.lrc'
+  },
+   {
+    name: '青春的颜色',
+      artist: 'YI',
+       url: './music/青春的颜色.mp3',
+      cover: './music/i.png',
+       lrc: './music/青春的颜色.lrc'
+
+}
+ ];
+
+// var remoteMusic = "./musiclist.json"
