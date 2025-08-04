3d380c9ba75fa0a6f1e4a57296e85711fe2afef9
琴 <212545318@qq.com>
2025-08-04 16:44:13 +0800

    Add files via upload

---
+++ music/he.js
@@ -0,0 +1,13 @@
+var userId = "25390779";
+var userServer = "netease";
+var userType = "playlist";
+ var localMusic = [{
+        name: '你笑起来真好看',
+      artist: '馨怡是个好学宝',
+       url: './music/你笑起来真好看.mp3',
+      cover: './music/Yi.jpg',
+       lrc: './music/你笑起来真好看.lrc'
+}
+ ];
+
+// var remoteMusic = "./musiclist.json"
